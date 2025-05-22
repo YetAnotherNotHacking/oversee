193d947da6932f77ff1fa3646c31dd20a7be5364
YetAnotherNotHacking <yetanotherdingus@gmail.com>
2025-05-22 15:11:14 +0200

    transition to tkinter base IDK

---
+++ src/initdata/headinit.py
@@ -0,0 +1,34 @@
+import os
+
+from tminus.getiplist import scrape_insecam_camera_urls
+from tminus.ip2locdownload import download_database
+from tminus.formatscrapeddata import format_file
+
+raw_scraped_data_file = "stream_links.txt" # From scraper
+processed_urls_file = "rawips.txt" # For program to read
+base_url = "http://www.insecam.org/en/byrating/"
+total_pages = 448
+database_url = "https://download.ip2location.com/lite/IP2LOCATION-LITE-DB1.CSV.ZIP"
+database_compressed_name = "IP2LOCATION-LITE-DB1.CSV.ZIP"
+database_uncompressed_name = "IP2LOCATION-LITE-DB1.CSV"
+
+def check_file_existance(file1, file2):
+    return os.path.isfile(file1), os.path.isfile(file2)
+
+def safe_remove(path):
+    if os.path.isfile(path):
+        os.remove(path)
+
+def initall():
+    raw_exists, processed_exists = check_file_existance(raw_scraped_data_file, processed_urls_file)
+    if not raw_exists or not processed_exists:
+        safe_remove(raw_scraped_data_file)
+        safe_remove(processed_urls_file)
+        print(f"Starting to scrape {base_url} for {total_pages} pages")
+        scrape_insecam_camera_urls(output_file=raw_scraped_data_file, base_url=base_url, total_pages=total_pages)
+        print("Done scraping for the ip list")
+        print("Starting to format the files")
+        format_file(raw_scraped_data_file, processed_urls_file)
+    print("Checking if the database is needed, downloading if it's missing")
+    download_database(database_url, database_compressed_name, database_uncompressed_name)
+    print("Done downloading the ip2loc database")
---
+++ src/initdata/ip2locdownload.py
@@ -0,0 +1,13 @@
+import requests
+import zipfile
+import os
+
+def download_database(DB_URL, DB_ZIP, DB_CSV):
+    if os.path.exists(DB_CSV):
+        return
+    if not os.path.exists(DB_ZIP):
+        r = requests.get(DB_URL)
+        with open(DB_ZIP, "wb") as f:
+            f.write(r.content)
+    with zipfile.ZipFile(DB_ZIP, 'r') as zip_ref:
+        zip_ref.extract(DB_CSV)
