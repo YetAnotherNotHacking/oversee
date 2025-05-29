e6f39fec5d3584fc176c8d5a33f7a7d2613ccb89
YetAnotherNotHacking <yetanotherdingus@gmail.com>
2025-05-29 21:13:01 +0200

    bruh

--- website/backend/.env.php
+++ website/backend/.env.php
@@ -1,13 +1,14 @@
 <?php
+// IF this gets commited, fuck gitignore.
 // Database Configuration
 define('DB_HOST', 'localhost:3306');
 define('DB_NAME', 'seetheworld');
 define('DB_USER', 'seetheworld');
-define('DB_PASS', 'thepoweroftheworld');
+define('DB_PASS', 'bruh');
 
 // Security Configuration
 define('ADMIN_PASSWORD_HASH', '30a831bfc93a7ee894e6408c3ffe226a');
 
 // API Keys
-define('PUBLIC_API_KEY', 'publicaccesstokenzareawesome');
-define('PRIVATE_API_KEY', 's3cr3tzofThew0rlDaretor3m14ns3cr3tf0r3v3r4nd3v3r');
+define('PUBLIC_API_KEY', 'bruh');
+define('PRIVATE_API_KEY', 'bruh');
